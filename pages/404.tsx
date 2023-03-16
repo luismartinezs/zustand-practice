71b1a86a14e38d4b83b4aae72c0e939f59b5b59f
Luis Martinez <luismartinezwebdev@gmail.com>
2023-03-16 13:52:10 +0000

    feat: funny naming

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 import { Heading } from "@chakra-ui/react";
 
-export default function FourOhFour() {
+export default function YouDontHaveTheRightOYouDontHaveTheRight() {
   return <Heading as="h1">404 - Page Not Found</Heading>;
 }
