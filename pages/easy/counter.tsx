c3b876321c295f89e32ca3538155c586d2bd69fb
Luis Martinez <luismartinezwebdev@gmail.com>
2023-03-16 05:43:22 +0000

    feat: counter store

--- pages/easy/counter.tsx
+++ pages/easy/counter.tsx
@@ -1,6 +1,12 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { Heading, Text, Button, VStack, HStack } from "@chakra-ui/react";
+import { useCounterStore } from "@/store/counter";
 
 export default function Counter() {
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
   return (
     <>
       <Head>
@@ -10,8 +16,15 @@ export default function Counter() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        Counter
+        <VStack>
+          <Heading as="h1">Counter</Heading>
+          <Text>Current value: {count}</Text>
+          <HStack>
+            <Button onClick={decrement}>Decrement</Button>
+            <Button onClick={increment}>Increment</Button>
+          </HStack>
+        </VStack>
       </main>
     </>
-  )
+  );
 }
