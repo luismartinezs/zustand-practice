96f52d9e35326ef802bdcd138e878a2008694a52
Luis Martinez <luismartinezwebdev@gmail.com>
2023-03-16 13:51:08 +0000

    feat: better header

---
+++ pages/404.tsx
@@ -0,0 +1,5 @@
+import { Heading } from "@chakra-ui/react";
+
+export default function FourOhFour() {
+  return <Heading as="h1">404 - Page Not Found</Heading>;
+}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,51 @@
-import { Flex, Link } from "@chakra-ui/react";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 
-const links = [
-  {
-    name: "Home",
-    href: "/",
-  },
-  {
-    name: "Counter",
-    href: "/easy/counter",
-  },
-  {
-    name: "Modal",
-    href: "/easy/modal",
-  },
-  {
-    name: "Array",
-    href: "/easy/array",
-  },
-];
+const links = {
+  easy: [
+    {
+      name: "Counter",
+      href: "/counter",
+    },
+    {
+      name: "Modal",
+      href: "/modal",
+    },
+    {
+      name: "Array",
+      href: "/array",
+    },
+  ],
+  medium: [
+    {
+      name: "Tasks",
+      href: "/tasks",
+    },
+  ],
+};
+
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 const Header = (): JSX.Element => {
   return (
     <header>
-      <Flex as="nav" gap={4}>
-        {links.map((item) => (
-          <Link as={NextLink} key={item.href} href={item.href} color="blue.100">
-            {item.name}
-          </Link>
+      <Flex as="nav" flexDir="column" gap={2}>
+        {Object.entries(links).map(([level, items]) => (
+          <Flex key={level} gap={4}>
+            <Text as="span">{capitalize(level)}: </Text>
+            {items.map((item) => (
+              <Link
+                as={NextLink}
+                key={item.href}
+                href={`/${level}${item.href}`}
+                color="blue.100"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </Flex>
         ))}
       </Flex>
     </header>
